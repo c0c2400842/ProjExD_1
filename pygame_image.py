a40515a99fd4d1b259bebe7c508e00639ea6ded0
c0c2400842 <c0c2400842@edu.teu.ac.jp>
2025-04-15 16:02:31 +0900

    練習8:背景画像を反転させてくっつけた

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")#背景画像を読みだして描画
+    bg_fliped_img = pg.transform.flip(bg_img, True, False)#練習8
     koukaton_img = pg.image.load("fig/3.png")#工科とんの画像を読み込む。練習1
     koukaton_img = pg.transform.flip(koukaton_img, True, False) #練習2 こうかとんの左右反転
     tmr = 0
@@ -19,7 +20,7 @@ def main():
         
         x = tmr #練習6
         screen.blit(bg_img, [-x, 0]) #画面サーフェスに座標ヨコ-x(変数),タテ0で画像を張り付けてね
-        screen.blit(bg_img, [-x+1600, 0]) #練習7
+        screen.blit(bg_fliped_img, [-x+1600, 0]) #練習7
         screen.blit(koukaton_img, [300, 200])  #練習4
         pg.display.update()
         tmr += 1        #タイマー変数
