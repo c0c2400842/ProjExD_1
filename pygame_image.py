aacf58ec2110d3982a17e5327dcb557f0009fe0c
c0c2400842 <c0c2400842@edu.teu.ac.jp>
2025-04-15 15:33:00 +0900

    練習2:こうかとん画像の読み込み

---
+++ pygame_image.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")#背景画像を読みだして描画
+    koukaton_img = pg.image.load("fig/3.png")#工科とんの画像を読み込む。練習1
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0]) #画面サーフェスに座標ヨコ0,タテ0で画像を張り付けてね
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
